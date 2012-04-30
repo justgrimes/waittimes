b4870d367f84c56fdf0c7d1ab5e61b66430d957f
Thomas Levine <tlevine@mnemonic.(none)>
2012-04-30 18:19:40 -0400

    istaonto

--- api/app.py
+++ api/app.py
@@ -6,6 +6,20 @@ urls = (
 )
 app = web.application(urls, globals())
 
+MIN_KEYS = set(['locationId', 'apikey', 'userId'])
+MAX_KEYS = set([
+    "locationId",
+    "appId",
+    "appUserId",
+    "time1",
+    "time2",
+    "time3",
+    "timediff21",
+    "timediff31",
+    "timediff32",
+    "appspecific",
+])
+
 class everything:
     def GET(self):
         'Query string like ?doc={saoehaostnoeau:asoetusoaestn,soe:soe} '
@@ -15,15 +29,19 @@ class everything:
         # Test this
         # doc['locationId']
 
-
         # Minimum keys
-        if not set(['locationId', 'apikey', 'userId']).issubset(doc.keys()):
+        if not MIN_KEYS.issubset(doc.keys()):
             raise ValueError('locationId, apikey and userId are required.')
-        elif 
+        elif MAX_KEYS.issuperset(doc.keys()):
+            raise ValueError('one of the keys you entered isn\'t allowed')
+
+        #"timeReceived": "2012-11-04 13:20:02 GMT-7",
 
         # Hack
         doc['appId'] = doc['apikey']
         del(doc['apikey'])
+
+        return json.dumps(doc)
 
     def POST(self, 
 
