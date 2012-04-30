e96fca5fa2cb3d020c3753cd71adc6869ba11a0c
Sam Edwards <sam@handstandtech.com>
2012-04-30 15:32:25 -0400

    Latest

--- mobile/script.js
+++ mobile/script.js
@@ -1,23 +1,49 @@
 var vip = {
+	locations : [],
+	location : undefined,
 	BASE_API_URL : "http://api.votinginfoproject.org/vip/3.0/",
+	addListClickListeners: function() {
+		$("ul#locations a").click(function(e) {
+			e.preventDefault();
+			alert('clicked!');
+			var idx = $(this).attr("idx");
+			console.log("index: " + idx);
+			vip.location = locations[idx];
+
+			var locationId = window.vip.location.id;
+			console.log("locationId" + locationId);
+
+			var url = "http://10.166.23.96:8080/location";
+
+			var data = {
+				apikey : 1,
+				locationId : locationId,
+				limit : 300
+			};
+
+			$.post(url, data, function(result) {
+				console.log(result);
+			}, "json");
+			return false;
+		});
+	},
 	vipCallback : function(result) {
-		var locations = result.locations;
+		vip.locations = result.locations;
 		$("#locations").empty();
 
-		for ( var i = 0; i < locations.length; i++) {
-			var location = locations[i];
+		for ( var i = 0; i < vip.locations.length; i++) {
+			var location = vip.locations[i];
 			var address = location.address;
 
-			var html = "<li><a href='#location'>";
+			var html = "<li><a href='#location' idx='" + i + "'>";
 			html += address.location_name;
 			if (address.line1 != '') {
 				html += "<br>" + address.line1;
 			}
-			
+
 			if (address.line2 != '') {
 				html += ", " + address.line2;
 			}
-
 
 			if (address.city != '') {
 				html += ", " + address.city;
@@ -30,13 +56,13 @@ var vip = {
 				html += " " + address.zip;
 			}
 
-
 			html += "</a></li>";
 			$("#locations").append(html);
 		}
 
 		// Force Listview Redraw
 		$("ul").listview("refresh");
+		addListClickListeners();
 		// $.mobile.hidePageLoadingMsg();
 	},
 	searchFormatted : function(formattedAddress) {
